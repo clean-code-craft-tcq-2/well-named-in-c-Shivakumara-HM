5a08024a1173fa5cb5b0551147915e9c8da088e0
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-11 21:16:00 +0530

    Update ColorFromPairNumber.c

--- ColorFromPairNumber.c
+++ ColorFromPairNumber.c
@@ -1,1 +1,15 @@
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
 
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
