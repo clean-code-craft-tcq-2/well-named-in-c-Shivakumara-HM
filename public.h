782c1fb63f9f63a0f4c9100d4616c9820558a789
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-12 23:32:19 +0530

    Update public.h

--- public.h
+++ public.h
@@ -22,3 +22,12 @@ typedef struct
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor);
+
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber);
