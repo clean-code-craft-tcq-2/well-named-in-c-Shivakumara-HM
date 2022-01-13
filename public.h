f4e9f63801088133d9c88ab6330b9dc3a8aa8e26
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-13 22:36:51 +0530

    Update public.h

--- public.h
+++ public.h
@@ -6,19 +6,16 @@ const char* MajorColorNames[] =
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 	
 const char* MinorColorNames[] = 
 {
     "Blue", "Orange", "Green", "Brown", "Slate"
 };
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-int pairNumber;
-char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 typedef struct 
 {
@@ -35,4 +32,4 @@ void testPairToNumber(
     enum MinorColor minor,
     int expectedPairNumber);
 
-void printRefernceManual()
+void printRefernceManual();
