ffdcf132266effef4fb1e045c9c90e9015602a9f
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-13 21:39:37 +0530

    Update getReferenceManual.c

--- getReferenceManual.c
+++ getReferenceManual.c
@@ -1,11 +1,24 @@
 #include <public.h>
 
-void printRefernceManual()
+const char* MajorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const char* MinorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+int numberOfMajorColors = sizeof(MajorColor) / sizeof(MajorColor[0]);
+int numberOfMinorColors = sizeof(MinorColor) / sizeof(MinorColor[0]);
+
+void getReferenceManual(void)
 {
-    for(pairNumber=1;pairNumber<=(numberOfMajorColors*numberOfMinorColors);pairNumber++)
-    {
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("color number: %d \t color pair: %s    \t Major color: %d \t Minor color: %d \n", pairNumber, colorPairNames, colorPair.majorColor+1, colorPair.minorColor+1);
-    }
+	int MajorColorIndex = 0, MinorColorIndex = 0, PairNumber = 0;
+	printf("\n---------------------------------------------\n");
+	printf("\tREFERENCE MANAUAL");
+	printf("\n---------------------------------------------\n");
+	printf("PAIR NO.\t MAJOR COLOR\t MINOR COLOR\n");
+	printf("\n---------------------------------------------\n");
+	for (MajorColorIndex = 0; MajorColorIndex < numberOfMajorColors; MajorColorIndex++)
+	{
+		for (MinorColorIndex = 0; MinorColorIndex < numberOfMinorColors; MinorColorIndex++)
+		{
+			printf("%d\t\t%s\t\t%s\n", ++PairNumber, MajorColor[MajorColorIndex], MinorColor[MinorColorIndex]);
+		}
+	}
 }
