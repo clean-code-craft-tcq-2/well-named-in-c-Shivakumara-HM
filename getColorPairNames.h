88cf0d164b9422257a8935926970985b3c51298d
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-15 14:42:47 +0530

    Update getColorPairNames.h

--- getColorPairNames.h
+++ getColorPairNames.h
@@ -1,7 +1,7 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 
