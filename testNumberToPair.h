5ac3752c40e716aac8e1e44ef9f7be75fed1ea1d
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-15 14:47:28 +0530

    Update testNumberToPair.h

--- testNumberToPair.h
+++ testNumberToPair.h
@@ -1,12 +1,12 @@
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+extern enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+extern enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
-const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+extern const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+extern const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
 
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+extern int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+extern int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 typedef struct 
 {
