036394a13cb03ba2a9282ebe61010870bc9c4393
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-15 12:10:12 +0530

    Create getReferenceModule.h

---
+++ getReferenceModule.h
@@ -0,0 +1,7 @@
+int PairNumber;
+
+const char* MajorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const char* MinorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+int numberOfMajorColors = sizeof(MajorColor) / sizeof(MajorColor[0]);
+int numberOfMinorColors = sizeof(MinorColor) / sizeof(MinorColor[0]);
