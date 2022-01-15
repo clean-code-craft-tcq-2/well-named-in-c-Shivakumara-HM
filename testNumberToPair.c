75fdc1c4a7ea7740fc7915b338fad3f5d8aa2687
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-15 12:55:34 +0530

    Create testNumberToPair.c

---
+++ testNumberToPair.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <assert.h>
+#include "testNumberToPair.h"
+
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
