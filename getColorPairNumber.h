1f35c3f957e009994343f6dc4ab46718396cbe61
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-15 14:52:04 +0530

    Update getColorPairNumber.h

--- getColorPairNumber.h
+++ getColorPairNumber.h
@@ -1,11 +1,11 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-extern const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
-extern const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-extern int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-extern int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 typedef struct 
 {
