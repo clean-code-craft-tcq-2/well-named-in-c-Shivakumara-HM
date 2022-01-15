1c2dc45d0d1a7230650a129c33683cabc4e17ad8
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-15 14:54:32 +0530

    Update getReferenceManual.h

--- getReferenceManual.h
+++ getReferenceManual.h
@@ -1,8 +1,8 @@
 
-extern const char* MajorColor[] = { "White", "Red", "Black", "Yellow", "Violet" };
-extern const char* MinorColor[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
 
-extern int numberOfMajorColors = sizeof(MajorColor) / sizeof(MajorColor[0]);
-extern int numberOfMinorColors = sizeof(MinorColor) / sizeof(MinorColor[0]);
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 void printReferenceManual(void);
