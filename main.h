043e65a5df72f1a51c5b7128e5ac0ada6d580918
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-15 13:59:15 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
-void printRefernceManual();
+extern void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+extern void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+extern void printRefernceManual();
