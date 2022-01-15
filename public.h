353964434e19f15155646ca571f7baef308e415c
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-15 12:18:49 +0530

    Update public.h

--- public.h
+++ public.h
@@ -1,20 +1,11 @@
 enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
 enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-const char* MajorColorNames[] = 
-{
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-	
-const char* MinorColorNames[] = 
-{
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
+const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 typedef struct 
@@ -23,13 +14,6 @@ typedef struct
     enum MinorColor minorColor;
 } ColorPair;
 
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor);
-
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber);
-
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber);
 void printRefernceManual();
