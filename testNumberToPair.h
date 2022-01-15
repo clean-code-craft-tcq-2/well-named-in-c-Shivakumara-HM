7007e052e08417acd9d1cd9cde47536b493c02d4
Shivakumara-HM <90903937+Shivakumara-HM@users.noreply.github.com>
2022-01-15 12:56:14 +0530

    Create testNumberToPair.h

---
+++ testNumberToPair.h
@@ -0,0 +1,17 @@
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+typedef struct 
+{
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor);
